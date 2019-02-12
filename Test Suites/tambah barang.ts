826372c9877aae4d69612780740c6786acfac154
Zul Kifli Abdulah <zulkifliabdulah@gmail.com>
2019-02-12 18:02:23 +0700

    Test Case Penambahan Barang Inventori

---
+++ Test Suites/tambah barang.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>tambah barang</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>4177e3e7-e464-471d-a218-17b4a4c57d9d</testSuiteGuid>
+   <testCaseLink>
+      <guid>5b438137-5201-42d9-a76a-65e95d80fad8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login RS</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1eb0b348-fcec-4412-905a-093025ebb0b6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Penambahan Barang Inventori</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
