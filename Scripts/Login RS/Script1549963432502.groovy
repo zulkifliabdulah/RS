5e5e2e603c4d922593d157e4149590374ec27f8a
Perdi Atmaja <atmaja.clan@gmail.com>
2019-02-12 16:41:43 +0700

    first time commit

---
+++ Scripts/Login RS/Script1549963432502.groovy
@@ -0,0 +1,25 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+
+Mobile.startApplication('/Users/admin/Downloads/app-staging-2.0.3.apk', true)
+
+Mobile.setText(findTestObject('android.widget.EditText0 - 081310634886'), '0812345', 0)
+
+Mobile.setText(findTestObject('android.widget.EditText1 - Kata Sandi'), '123456', 0)
+
+Mobile.tap(findTestObject('android.view.View1'), 0)
+
+//Mobile.closeApplication()
+
